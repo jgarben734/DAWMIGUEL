26a0ecb4f966b814e36365b36bec13012f17ffc6
Miguel <josemiguelgarciabenitez@gmail.com>
2023-09-22 17:36:25 +0200

    TAREA DEL PROYECTO 3_CLASE DE NUMERO

---
+++ Programación/Workspaces/proyecto 3 clase numero/src/proyecto_3_clase_numeros.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class proyecto_3_clase_numeros {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d1 = 10;
+		double d2 = 20;
+		double resul1;
+		double resul2;
+		double resul3;
+		
+		
+		
+		//1.
+		resul1 = 4-d1*2;
+		System.out.println("Restamos 4 al doble de d1:"+resul1);
+		//2.
+		 resul2 = (-4+d1)*2;
+		System.out.println("Restamos 4 a d1 y calculamos el doble:"+ resul2);
+		//3.
+		resul3 = (2+d1)/12 + d2;
+		System.out.println("Sumamos 2 a d1 y dividimos por 12, a todo ello sumamos d2:"+ resul3);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
