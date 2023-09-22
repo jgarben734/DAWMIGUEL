da5aaea011650a9bf951ffe4fc511f24d381e68e
Miguel <josemiguelgarciabenitez@gmail.com>
2023-09-22 16:58:48 +0200

    proyecto 2 variables

--- Programación/Workspaces/hola mundo/src/holamundo1.java
+++ Programación/Workspaces/hola mundo/src/holamundo1.java
@@ -22,6 +22,7 @@ public class holamundo1 {
 		
 		
 		
+		
 	}
 
 }
---
+++ Programación/Workspaces/proyecto2/src/proyecto2/proyecto2.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package proyecto2;
+
+/**
+ * 
+ */
+public class proyecto2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Creacion de las variables
+		int i = 10;
+		double d = 3.14;
+		char c1 = 'a'; 
+		char c2 = 'A';
+		boolean encontrado = true;
+		String mensaje  = "Bienvenido a JAVA";
+			
+		// MOSTRAR EN CONSOLA 
+		
+		System.out.println(" La variable i es de tipo entero y su valor es : " + i);		
+		System.out.println(" La variable double es de tipo entero y su valor es : " + d);
+		System.out.println(" La variable char es de tipo entero y su valor es : " + c1);
+		System.out.println(" La variable char  es de tipo entero y su valor es : "+ c2);
+		System.out.println(" La variable boolean es de tipo entero y su valor es : "+ encontrado);
+		System.out.println(" La variable String es de tipo entero y su valor es : "+ mensaje);
+	}
+	
+	
+}
