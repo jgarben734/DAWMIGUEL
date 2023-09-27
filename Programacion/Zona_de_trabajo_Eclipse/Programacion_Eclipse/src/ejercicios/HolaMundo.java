d40b43d3280f77a145f0c8f44ed34d80387ba4b6
Miguel <josemiguelgarciabenitez@gmail.com>
2023-09-27 16:51:53 +0200

    Elimino todo y vuelvo a organizar desde 0

---
+++ Programacion/Zona_de_trabajo_Eclipse/Programacion_Eclipse/src/ejercicios/HolaMundo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ejercicios;
+
+/**
+ * 
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
