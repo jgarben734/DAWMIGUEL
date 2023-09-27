28c22359824b4f678c78f0399e1e8f7525b1ac96
Miguel <josemiguelgarciabenitez@gmail.com>
2023-09-27 16:04:29 +0200

    Actualización  de Boletín de ejercicio etc

---
+++ Programación/Workspaces/Boletin2/src/Boletin2_ejercicios.java
@@ -0,0 +1,90 @@
+
+public class Boletin2_ejercicios {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//3. Boletin de ejercicios
+String nombre = "Jose Miguel Garcia Benitez";
+System.out.println(nombre);
+
+
+
+
+	//4. 
+		System.out.println("*");
+		System.out.println("***");
+		System.out.println("*****");	
+		System.out.println("*******");
+		System.out.println("*********");
+		
+		
+		
+		
+		System.out.println(
+				"\t\t\t"
+				+ "*\n"
+				+ "\t\t\t"
+				+ "***\n"
+				+ "\t\t\t\t"
+				+ "****\n");
+		
+		
+		/*
+		 * 5.Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+los valores 144 y 999 respectivamente. A continuación, muestra por pantalla el
+valor de cada variable, la suma, la resta, la división y la multiplicación.
+		 */
+		int x, y;
+		x = 144;
+		y=999;
+	System.out.println("La variable x vale:" + x +" .\n" + 
+		"La variable y vale: " + y + ".\n" +
+			"x + y = "+ ( x + y ) + ".\n" +
+		
+		
+	/*
+	 * 5. Crea las variables nombre, direccion y telefono y asígnales los valores
+correspondientes. Muestra los valores de esas variables por pantalla en la
+misma línea separados por punto y coma (;).
+	 */
+
+	 
+	  
+public static void main(String[] args) {
+		
+	}
+	  String nombre = (Miguel);
+	  String direccion =(Barbate);
+	  String telefono = (555 666 777);
+	  System.out.println("Miguel, Barbate, 555-666-777");
+	  
+	  
+	  
+	  
+	
+	  
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+		
+		
+			
+
+
+
+
+
+
+
+
+
+	}
+
+}
