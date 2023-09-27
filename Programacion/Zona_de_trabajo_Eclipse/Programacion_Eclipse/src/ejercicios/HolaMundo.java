ddd870457f20246aa099e7ac21ecd8767bf2f509
Miguel <josemiguelgarciabenitez@gmail.com>
2023-09-27 17:25:53 +0200

    Realizo el ejercicio de Clase sobre (Hola Mundo)

--- Programacion/Zona_de_trabajo_Eclipse/Programacion_Eclipse/src/ejercicios/HolaMundo.java
+++ Programacion/Zona_de_trabajo_Eclipse/Programacion_Eclipse/src/ejercicios/HolaMundo.java
@@ -7,13 +7,20 @@ package ejercicios;
  * 
  */
 public class HolaMundo {
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Comentario de una sola línea
+		/*
+		 * Comentario de 
+		 * varias líneas
+		 */
+		System.out.println("Hola Mundo!");
+		
 	}
 
 }
