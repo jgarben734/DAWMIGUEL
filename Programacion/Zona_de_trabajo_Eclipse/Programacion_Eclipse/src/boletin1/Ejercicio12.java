1150fa80c65e75e46f29ebcadd481e0628801bd3
Miguel <josemiguelgarciabenitez@gmail.com>
2023-09-29 17:32:48 +0200

    actualizar boletin1 programacion para hacer las actividades.

---
+++ Programacion/Zona_de_trabajo_Eclipse/Programacion_Eclipse/src/boletin1/Ejercicio12.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package boletin1;
+
+/**
+ * 
+ */
+public class Ejercicio12 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
