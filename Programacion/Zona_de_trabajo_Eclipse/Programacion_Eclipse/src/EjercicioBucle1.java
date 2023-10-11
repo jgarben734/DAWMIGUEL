93cfc73392d95af120be98f3a52fd7f9b8396652
Miguel <josemiguelgarciabenitez@gmail.com>
2023-10-11 18:26:52 +0200

    actualizacion

---
+++ Programacion/Zona_de_trabajo_Eclipse/Programacion_Eclipse/src/EjercicioBucle1.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class EjercicioBucle1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner(System.in);
+		boolean correcto = false;
+		do {
+			System.out.println("Eligue una operacion"
+					+ "\n1.Sumar"
+					+ "\n2.Resta"
+					+ "\n3.Multiplicar"
+					+ "\n4.Dividir");
+			int opcion = scan.nextInt();
+			System.out.println("Introduce el primer operando");
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		scan.close();
+		
+				
+		
+		
+		
+		
+		
+		
+	}
+
+}
