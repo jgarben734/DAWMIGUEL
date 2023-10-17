f3ba1fb5f738aed54605e1104399c1617b61a056
jgarben734 <josemiguelgarciabenitez@gmail.com>
2023-10-17 12:38:11 +0200

    actualizacion

---
+++ Programación/Curso_Java_Tutorial/src/EjercicioI_Java.java
@@ -0,0 +1,27 @@
+public class EjercicioI_Java {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String nombre = "Ana";
+		System.out.println(nombre);
+		int edad = 7;
+		System.out.println(edad);
+		double salario = 200;
+		System.out.println(salario);
+		boolean carnet = true;
+		System.out.println(carnet);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
