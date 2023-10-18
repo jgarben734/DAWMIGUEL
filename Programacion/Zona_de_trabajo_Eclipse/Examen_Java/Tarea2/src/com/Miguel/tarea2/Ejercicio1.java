a049f0737606df9b55e45a8eb6a2d1b08be8bc4b
Miguel <josemiguelgarciabenitez@gmail.com>
2023-10-18 15:34:48 +0200

    actualizacion

---
+++ Programacion/Zona_de_trabajo_Eclipse/Examen_Java/Tarea2/src/com/Miguel/tarea2/Ejercicio1.java
@@ -0,0 +1,22 @@
+package com.Miguel.tarea2;
+
+import java.util.Iterator;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println();
+		
+			
+		
+		
+			
+		
+		
+
+	}
+
+}
