10b7bf9008ede1bd87f179dfaf016a82c088b251
Miguel <josemiguelgarciabenitez@gmail.com>
2023-10-31 19:52:00 +0100

    ok

--- Programacion/Workspaces/Pasos desde 0/src/ejercicioclase/unidad03/Ejercicioclase01.java
+++ Programacion/Workspaces/Pasos desde 0/src/ejercicioclase/unidad03/Ejercicioclase01.java
@@ -1,10 +1,31 @@
 package ejercicioclase.unidad03;
 
+// Si  nos encontramos un asterisco en importar las clases es que tenemos todos los detalles de las clases de esa importación (*;)
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class Ejercicioclase01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
+		LocalDate ahora = LocalDate.now();
+		System.out.println("Fecha con LocalDate. " + ahora);
+		DateTimeFormatter formatLocalDate = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		System.out.println("Fecha con LocalDate formateada: " + ahora.format(formatLocalDate));
+		
+		
+		
+		
+		
+		
+		
+		
+		Date hoy = new Date();
+		System.out.println("Fecha con Date: "+ hoy);
+		
 		
 		
 		
