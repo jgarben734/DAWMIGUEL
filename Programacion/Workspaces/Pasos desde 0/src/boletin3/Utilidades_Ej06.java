16b2ed688c9942095e4a5502687d7775a639e6db
Miguel <josemiguelgarciabenitez@gmail.com>
2023-11-03 17:29:35 +0100

    añado ejercicio  del boletin 3 que falta por terminar.

---
+++ Programacion/Workspaces/Pasos desde 0/src/boletin3/Utilidades_Ej06.java
@@ -0,0 +1,17 @@
+package boletin3;
+
+public class Utilidades_Ej06 {
+	public static int introduceInt(String cadena)
+	int res;
+	try {
+		
+	} catch  (NumberFormatException e) {
+		Integer i = Integer.parseInt(cadena);
+			res=i;
+			
+	}
+	
+	return res;	
+	
+
+}
