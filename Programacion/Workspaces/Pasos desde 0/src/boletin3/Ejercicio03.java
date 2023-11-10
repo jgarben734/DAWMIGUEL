76ae951e94f6e92a2c3a4a219dbb75b74d03bc90
Miguel <josemiguelgarciabenitez@gmail.com>
2023-11-10 17:30:20 +0100

    ok

---
+++ Programacion/Workspaces/Pasos desde 0/src/boletin3/Ejercicio03.java
@@ -0,0 +1,21 @@
+package boletin3;
+
+import java.util.Scanner;
+
+public class Ejercicio03 {
+	
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a =0,  b= 0;
+		float resul = 0f;
+		System.out.println("Vamos a hacer la operación A/B. \n Tendrás que introducir dos números enteros. \n Introduce A");
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
